b87e33295711d0a2cff171545f470a1f3be3a798
Yangjialin1989 <729850713@qq.com>
2019-07-14 10:37:50 +0800

    Signed-off-by: Yangjialin1989 <729850713@qq.com> 添加首页模板-九宫格及动画效果。

--- 02.vue-cms/src/main.js
+++ 02.vue-cms/src/main.js
@@ -1,22 +1,39 @@
 //入口文件
 import Vue from 'vue'
 
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2安装路由模块
+Vue.use(VueRouter)
+
+//2.1 导入 vue-resource 
+import VueResource from 'vue-resource'
+//2.2 安装vue-resource
+Vue.use(VueResource)
+
+
 //导入MUI的样式
 import './lib/mui/css/mui.css'
 import './lib/mui/css/icons-extra.css'
 import './lib/mui/fonts/mui-icons-extra.ttf'
 
 //按需导入Mint-UI组件
-import { Header } from 'mint-ui'
+import { Header,Swipe,SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
 
+//1.3 导入自己的router.js 路由模块
+import router from './router.js'
 
 //导入App根组件
 import app from './App.vue'
 
 var vm = new Vue({
 	el:'#app',
-	render:c=>c(app)
+	render:c=>c(app),
+	//1.4挂载路由到VM实例上
+	router
 })
 
 
--- 02.vue-cms/src/router.js
+++ 02.vue-cms/src/router.js
@@ -1,12 +1,25 @@
 import VueRouter from 'vue-router'
 
-
+//导入对应的路由组件
+import HomeContainer from    './components/tabbar/HomeContainer.vue'
+import MemberContainer from  './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from  './components/tabbar/SearchContainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
   routes: [
-
-  ]
+		//3.1配置路由规则
+		//3.1.1路由重定向到home
+		{ path:'/', redirect:'/home'},
+		//3.1.2路由通用配置
+		{ path: '/home',component: HomeContainer},
+		{ path: '/member',component: MemberContainer},
+		{ path: '/shopcar',component: ShopcarContainer},
+		{ path: '/search',component: SearchContainer}
+  ],
+	//覆盖默认的路由高亮的类active-link-class 
+	linkActiveClass:'mui-active'
 })
 
 // 把路由对象暴露出去
